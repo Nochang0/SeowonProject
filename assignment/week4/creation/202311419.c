588c11dd625c7b14ffad04c7cf6d8eabff2333fc
ㅇㅇ <>
2023-11-30 17:01:25 +0000

    프로젝트 완성

--- assignment/week4/creation/202311419.c
+++ assignment/week4/creation/202311419.c
@@ -1,8 +1,34 @@
 /*
-* 프로그램 내용:  (창작 문제)
+* 프로그램 내용: 이름과 주민등록번호를 받아 성별과 같이 출력하는 프로그램 (창작 문제)
 * 개발자: 양혜성 (202311419)
 */
 
+
+#include <stdio.h>
+
+const char* c_gender(char digit) {
+    return (digit == '1' || digit == '3') ? "남자" : "여자";
+}
+
+int main() {
+    char name[50];
+    char number[14];
+
+    printf("이름을 입력: "); 
+    scanf("%49s", name, sizeof(name));
+
+    printf("주민등록 번호: ");
+    scanf("%13s", number, sizeof(number));
+
+    printf("이름: %s\n", name);
+
+    printf("주민등록번호 %.*s - *******\n", 6, number);
+
+    const char* gender = c_gender(number[6]);
+    printf("성별: %s\n", gender);
+
+    return 0;
+}
 
 
 // cd /workspace/SeowonCProject/assignment/week4/creation/ && gcc ./202311419.c -o /tmp/a.out -lm && clear && /tmp/a.out
